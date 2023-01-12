cd6f5c8f60038c7bdeeb237fdc98e156e003d503
Mikil Foss <41125745+MikilFoss@users.noreply.github.com>
2023-01-12 15:35:27 -0600

    added group feedback

--- public/groupManager.js
+++ public/groupManager.js
@@ -1,5 +1,6 @@
 const createGroupButton = document.getElementById('groupmake');
 const joinGroupButton = document.getElementById("groupjoin");
+const errorText = document.getElementById("GroupError")
 
 const userID = document.getElementById('userID').innerHTML;
 
@@ -14,11 +15,13 @@ createGroupButton.addEventListener('click', async () => {
         headers: {'Content-Type': 'application/json'},
         body: JSON.stringify({ groupName: groupName, userID: userID })
     }
-      const response = await fetch('/createGroup', options);
-      const data = await response.json();
-      console.log(data.message);
+        const response = await fetch('/createGroup', options);
+        const data = await response.json();
+        console.log(data.message);
+        errorText.innerHTML = data.message;
   } catch (err) {
       console.error(err);
+      errorText.innerHTML = err;
   }
 });
 
@@ -31,11 +34,13 @@ joinGroupButton.addEventListener('click', async () => {
             headers: {'Content-Type': 'application/json'},
             body: JSON.stringify({ groupName: groupName, userID: userID })
         }
-          const response = await fetch('/joinGroup', options);
-          const data = await response.json();
-          console.log(data.message);
+            const response = await fetch('/joinGroup', options);
+            const data = await response.json();
+            console.log(data.message);
+            errorText.innerHTML = data.message;
       } catch (err) {
           console.error(err);
+          errorText.innerHTML = err;
       }
 
 });
